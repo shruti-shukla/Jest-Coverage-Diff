77a741c2de97bec7671ff66fe636fc7855045f00
Shruti Shukla <41375094+shruti-shukla@users.noreply.github.com>
2023-02-15 07:08:35 +0000

    Adding OSSF action

---
+++ .github/actions/checks/index.js
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {execSync} from 'child_process'
+
+const repoName = github.context.repo.repo
+const repoOwner = github.context.repo.owner
+const token = core.getInput('token')
+const checkName = core.getInput('checkName')
+// execSync('curl -OL https://github.com/ossf/scorecard/archive/refs/tags/v4.10.2.zip')
+// if(token) {
+//     execSync(`export GITHUB_AUTH_TOKEN=${token}`)
+// }
+console.log(`Repo Name: ${repoName}`)
+console.log(`Repo Owner: ${repoOwner}`)
+var result = 'Unable to run OSSF checks'
+if(checkName) {
+    result = execSync(` export GITHUB_AUTH_TOKEN=${token} ; scorecard --repo=github.com/${repoOwner}/${repoName} --checks=${checkName}`).toString();
+} else {
+    result = execSync(` export GITHUB_AUTH_TOKEN=${token} ; scorecard --repo=github.com/${repoOwner}/${repoName}`).toString();
+}
+console.log(result);
